8df259fe421de830c701d3c19c567572e99750aa
zmulji <znmulji1@gmail.com>
2019-09-03 16:54:33 -0400

    updated leaderboard page, added new components

--- client/src/pages/Leaderboard/Leaderboard.js
+++ client/src/pages/Leaderboard/Leaderboard.js
@@ -1,12 +1,16 @@
 import React from "react";
 import NavTabs from "../../components/NavTabs/NavTabs";
+import Table from "../../components/Table/Table";
+import Entry from "../../components/Entry/Entry";
 import './Leaderboard.css';
+
+import users from "../../fakeUsers.json";
 
 
 class Leaderboard extends React.Component {
 
   state = {
-    
+    users
   }
 
   
@@ -19,10 +23,20 @@ class Leaderboard extends React.Component {
 
           <h1>leaderboard</h1>
           <span className="instructions">check in on your friends...</span>
-          
+            
 
           <div>
-            
+            <Table>
+              {this.state.users.map(user => {
+                return(
+                  <Entry
+                    position={user.id}
+                    username={user.username}
+                    score={user.score}
+                  />
+                )
+              })}
+            </Table>
           </div>
           
 
