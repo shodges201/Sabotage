e7071f1ac701b8e5d834cd23a2af0a31813dd7f5
Backslash <lanceadrian13@gmail.com>
2019-09-04 10:54:18 -0400

    added bcrypt

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt-nodejs");
 
 const userSchema = new Schema({
   userName: { type: String, required: true },
@@ -9,4 +10,15 @@ const userSchema = new Schema({
 
 const User = mongoose.model("User", userSchema);
 
+  // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
+  User.prototype.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+  // Hooks are automatic methods that run during various phases of the User Model lifecycle
+  // In this case, before a User is created, we will automatically hash their password
+  User.addHook("beforeCreate", function(user) {
+    console.log('before create');
+    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+    console.log(user.password);
+  });
 module.exports = User;
