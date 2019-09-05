61aa98985f61c4935b00c4ab5924236e7ceb8570
Quinton Smith <smith.quinton42@gmail.com>
2019-09-05 10:53:46 -0400

    roulette wheel bigger

--- client/src/components/pages/Wheel/Roulette.js
+++ client/src/components/pages/Wheel/Roulette.js
@@ -30,7 +30,8 @@ class Roulette extends React.Component {
   
     static defaultProps = {
       options:  ['Lose Points', 'Gain Points', 'Steal Points', 'Give Points', 'Wild'],
-      baseSize: 275,
+      // baseSize: 275,
+      baseSize: 400,
       spinAngleStart: Math.random() * 10 + 10,
       spinTimeTotal: Math.random() * 3 + 4 * 1000,
     };
